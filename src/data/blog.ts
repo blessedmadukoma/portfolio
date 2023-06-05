3e714a3f4f1edf32895b10003db2b58ff4fe4315
blessedmadukoma <blessedmadukoma@gmail.com>
2023-06-05 21:55:43 +0100

    updated blog links

--- src/data/blog.ts
+++ src/data/blog.ts
@@ -2,32 +2,32 @@ export const BLOGS = [
   {
     title: "Deploy a Go RESTful API to Koyeb: an alternative to Heroku",
     description: "Deploy a Go RESTful API to Koyeb: an alternative to Heroku",
-    link: "https://blog.mblessed.tech/deploy-go-api-to-koyeb",
+    link: "https://mblessed.hashnode.dev/deploy-go-api-to-koyeb",
     views: "5000",
   },
   {
     title: "How I deploy my Golang API to Heroku",
     description: "How I deploy my Golang API to Heroku",
-    link: "https://blog.mblessed.tech/how-i-deploy-my-golang-api-to-heroku",
+    link: "https://mblessed.hashnode.dev/how-i-deploy-my-golang-api-to-heroku",
     views: "5000",
   },
   {
     title: "Serving NextJS with Go",
     description: "",
-    link: "https://blog.mblessed.tech/serving-nextjs-with-go",
+    link: "https://mblessed.hashnode.dev/serving-nextjs-with-go",
     views: "5000",
   },
   {
     title: "Go Embed: Embed your HTML frontend in Golang",
     description: "",
-    link: "https://blog.mblessed.tech/go-embed-embed-your-html-frontend-in-golang",
+    link: "https://mblessed.hashnode.dev/go-embed-embed-your-html-frontend-in-golang",
     views: "5000",
   },
   {
     title:
       "Getting started with Golang Web Application and Authentication Part 1",
     description: "",
-    link: "https://blog.mblessed.tech/getting-started-with-golang-web-application-and-authentication-part-1",
+    link: "https://mblessed.hashnode.dev/getting-started-with-golang-web-application-and-authentication-part-1",
     views: "5000",
   },
 ];
