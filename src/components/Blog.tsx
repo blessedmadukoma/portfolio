a45c3d1a556136ef9289b5d9c751c0f5af8eea32
blessedmadukoma <blessedmadukoma@gmail.com>
2023-06-05 21:55:04 +0100

    blog section

--- src/components/Blog.tsx
+++ src/components/Blog.tsx
@@ -1,7 +1,35 @@
+import { BLOGS } from "@/data/blog";
 import React from "react";
 
 const Blog = () => {
-  return <div className="py-24">Blog</div>;
+  const blogs = BLOGS;
+
+  return (
+    <div className="md:my-24 mx-4 md:mx-6">
+      <div className="text-start mx-5 md:mx-4 lg:mx-8 mb-8 text-3xl font-semibold">
+        Blogs
+      </div>
+      <div className=" grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-3">
+        {blogs.map((blog, id) => (
+          <a key={id} href={blog.link} target="_blank">
+            <div className="text-start bg-[#18173B] p-4 rounded-xl h-[290px] mx-auto">
+              <div className="text-base font-semibold md:text-xl mb-4 h-2/4">
+                {blog.title}
+              </div>
+
+              <div className="font-extralight md:font-light mb-10 text-sm h-1/4">
+                {blog.description}
+              </div>
+
+              <div className="text-sm text-[#FFFFFF]/[0.6] h-1/4">
+                {blog.views} views
+              </div>
+            </div>
+          </a>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Blog;
--- src/data/blog.ts
+++ src/data/blog.ts
@@ -3,26 +3,31 @@ export const BLOGS = [
     title: "Deploy a Go RESTful API to Koyeb: an alternative to Heroku",
     description: "Deploy a Go RESTful API to Koyeb: an alternative to Heroku",
     link: "https://blog.mblessed.tech/deploy-go-api-to-koyeb",
+    views: "5000",
   },
   {
     title: "How I deploy my Golang API to Heroku",
     description: "How I deploy my Golang API to Heroku",
     link: "https://blog.mblessed.tech/how-i-deploy-my-golang-api-to-heroku",
+    views: "5000",
   },
   {
     title: "Serving NextJS with Go",
     description: "",
     link: "https://blog.mblessed.tech/serving-nextjs-with-go",
+    views: "5000",
   },
   {
     title: "Go Embed: Embed your HTML frontend in Golang",
     description: "",
     link: "https://blog.mblessed.tech/go-embed-embed-your-html-frontend-in-golang",
+    views: "5000",
   },
   {
     title:
       "Getting started with Golang Web Application and Authentication Part 1",
     description: "",
     link: "https://blog.mblessed.tech/getting-started-with-golang-web-application-and-authentication-part-1",
+    views: "5000",
   },
 ];
