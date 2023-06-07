830d274a9c406b9d992a09675798cb3b2e9ad13d
blessedmadukoma <blessedmadukoma@gmail.com>
2023-06-07 19:24:14 +0100

    reduced the height, removed the views

--- src/components/Blog.tsx
+++ src/components/Blog.tsx
@@ -10,7 +10,16 @@ const Blog = () => {
       <div className=" grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-3">
         {blogs.map((blog, id) => (
           <a key={id} href={blog.link} target="_blank">
-            <div className="text-start bg-[#18173B] p-4 rounded-xl h-[290px] mx-auto">
+            <div className="text-start bg-[#18173B] p-4 rounded-xl h-[240px] mx-auto">
+              <div className="text-base font-semibold md:text-xl mb-4 h-2/4">
+                {blog.title}
+              </div>
+
+              <div className="font-extralight md:font-light mb-10 text-sm h-1/4">
+                {blog.description}
+              </div>
+            </div>
+            {/* <div className="text-start bg-[#18173B] p-4 rounded-xl h-[290px] mx-auto">
               <div className="text-base font-semibold md:text-xl mb-4 h-2/4">
                 {blog.title}
               </div>
@@ -19,10 +28,10 @@ const Blog = () => {
                 {blog.description}
               </div>
 
-              <div className="text-sm text-[#FFFFFF]/[0.6] h-1/4">
+               <div className="text-sm text-[#FFFFFF]/[0.6] h-1/4">
                 {blog.views} views
               </div>
-            </div>
+            </div> */}
           </a>
         ))}
       </div>
