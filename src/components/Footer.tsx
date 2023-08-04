6c719a7ab845ba4b66a9dd98aa1fc180135fbc8d
blessedmadukoma <blessedmadukoma@gmail.com>
2023-08-04 18:22:54 +0100

    updated dependencies, fixed rotation error

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -87,7 +87,7 @@ const Footer = () => {
         <div className="text-[#939196] text-center flex flex-col items-center">
           <div className="w-40 md:w-56 relative">
             <img src="./icons/memoji.png" alt="" />
-            <div className="absolute -top-1 right-[14px] md:top-7 md:right-[30px] animate-spin-slow">
+            <div className="absolute -top-1 right-[14px] md:top-7 md:right-[3rem] animate-spin-slow">
               {width < 768 ? (
                 <NameIcon width="130" />
               ) : (
