578e146004263cea52a9c8bf93ca51d1f09f3ac1
blessedmadukoma <blessedmadukoma@gmail.com>
2023-08-27 20:37:09 +0100

    added hover and underline

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -13,29 +13,46 @@ const Navbar = () => {
 
   return (
     <nav className="mt-24 py-8 sticky top-0 md:top-[64px] z-0 md:z-0 backdrop-blur-sm border-[#292844] border-y-[0.8px]">
-      <div className="flex justify-evenly text-[#A7A4A5] text-sm md:text-base">
-        <a href="/" className={`${isHome ? `hidden` : "visible"}`}>
+      <div className="flex justify-evenly text-[#d1d1d1] text-sm md:text-base">
+        <a
+          href="/"
+          className={`${isHome ? `hidden` : "visible"} hover:text-white`}
+        >
           <div>Home</div>
         </a>
 
         <a
           href="#projects"
-          className={`${pathname == "#projects" ? active : ""}`}
+          className={`${
+            pathname == "#projects" ? active : ""
+          } hover:text-white hover:underline`}
         >
           <div>Projects</div>
         </a>
 
-        <a href="#tools" className={`${pathname == "#tools" ? active : ""}`}>
+        <a
+          href="#tools"
+          className={`${
+            pathname == "#tools" ? active : ""
+          } hover:text-white hover:underline`}
+        >
           <div>Tools</div>
         </a>
 
-        <a href="#blog" className={`${pathname == "#blog" ? active : ""}`}>
+        <a
+          href="#blog"
+          className={`${
+            pathname == "#blog" ? active : ""
+          } hover:text-white hover:underline`}
+        >
           <div>Blog</div>
         </a>
 
         <a
           href="#contact"
-          className={`${pathname == "#contact" ? active : ""}`}
+          className={`${
+            pathname == "#contact" ? active : ""
+          } hover:text-white hover:underline`}
         >
           <div>Contact</div>
         </a>
