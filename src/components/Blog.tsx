9e9e7d36876e2f5e119c768424081fdb9d95e095
Blessed Madukoma <blessedmadukoma@gmail.com>
2025-01-01 00:16:36 +0200

    fix broken link

--- src/components/Blog.tsx
+++ src/components/Blog.tsx
@@ -57,7 +57,7 @@ query FetchAllPosts($host: String!) {
 // `);
 
 function truncateText(text: any, maxLength: number) {
-  if (text.length > maxLength) {
+  if (text?.length > maxLength) {
     return text.substring(0, maxLength) + "...";
   }
   return text;
