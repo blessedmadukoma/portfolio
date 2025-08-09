0ca1e303fb6af1f8a6ca1fdeba1152cb2595cba6
blessedmadukoma <blessedmadukoma@gmail.com>
2025-08-09 18:31:45 +0100

    feat: commenced projects section

--- migration/app/components/navs/projects.vue
+++ migration/app/components/navs/projects.vue
@@ -1,5 +1,25 @@
 <template>
   <section class="space-y-3">
-    <h2>Why I build:</h2>
+    <h2 class="text-sm tracking-wide">
+      <span class="font-bold">Why I build: </span>
+      <span>I build because...</span>
+    </h2>
+
+    <div>
+      <div>
+        <NuxtLink
+          to="#"
+          class="flex items-center space-x-4 hover:bg-gray-100 dark:hover:bg-gray-800 py-4 rounded-lg"
+        >
+          <p>Logo</p>
+          <div class="px-2">
+            <p>project Name - <span>brief text</span></p>
+            <p>what it is about, what it achieved etc</p>
+            <p>tools used</p>
+          </div>
+        </NuxtLink>
+      </div>
+    </div>
   </section>
 </template>
+Projects
