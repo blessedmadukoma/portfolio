9eedb1c9bfeb7388b575e9e3a40fad14a95d3eb5
Blessed Madukoma <blessedmadukoma@gmail.com>
2026-05-13 08:49:40 +0200

    add vercel analytics

--- app/app.vue
+++ app/app.vue
@@ -1,10 +1,10 @@
 <script setup lang="ts">
-  // import Analytics from "@vercel/analytics/nuxt";
+  import Analytics from "@vercel/analytics/nuxt";
 </script>
 
 <template>
   <div>
-    <!-- <Analytics /> -->
+    <Analytics />
     <!-- <main
       class="bg-white dark:bg-primary-blue text-gray-900 dark:text-gray-100 theme-transition"
       style="padding-top: 0; padding-bottom: 0"
