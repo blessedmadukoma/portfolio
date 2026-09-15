0e1b8f697647ca79a136d2e95f73a169fd2f99d3
Blessed Madukoma <blessedmadukoma@gmail.com>
2026-09-15 11:00:09 +0200

    fix(blog): drop orphaned separator in post meta

--- app/pages/blog/[slug].vue
+++ app/pages/blog/[slug].vue
@@ -119,7 +119,6 @@
               <span v-if="post.date">{{ formatDate(post.date as string) }}</span>
               <span v-if="post.date">·</span>
               <span v-if="post.readingTime">{{ post.readingTime }} min read</span>
-              <span v-if="post.readingTime">·</span>
               <!-- <span>{{ views ?? 0 }} {{ (views ?? 0) === 1 ? 'view' : 'views' }}</span> -->
             </div>
           </div>
